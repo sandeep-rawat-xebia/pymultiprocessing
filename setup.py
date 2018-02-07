4747d48b86d6ba17c83f3078e3f980bcc7e3e135
Sandeep <sandeeprawat@xebia.com>
2018-02-07 23:34:18 +0530

    changed module name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
-  name = 'multiprocessing',
-  packages = ['multiprocessing'], # this must be the same as the name above
+  name = 'pymultiprocessing',
+  packages = ['pymultiprocessing'], # this must be the same as the name above
   version = '0.1',
   description = 'multiprocessing package of python 2.7 ',
   author = 'Sandeep Rawat',
   author_email = 'san55577@gmail.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  url = 'https://github.com/sandeep-rawat-xebia/pymultiprocessing.git', # use the URL to the github repo
+  download_url = 'https://github.com/sandeep-rawat-xebia/pymultiprocessing/releases/download/0.1/pymultiprocessing-0.1.tar.gz', # I'll explain this in a second
   keywords = ['multiprocessing', 'python', 'jython'], # arbitrary keywords
   classifiers = [],
 )
